3feab515a338076239fdd5fd20a9467fbfeef78c
Siddharth Kumar <sidkumar@live.com>
2015-04-13 10:40:04 -0500

    Housekeeping

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ __author__ = 'Siddharth Kumar'
 import re
 
 class GRB:
+
     def __init__(self,name,ra,dec):
+        
         self.NAME = str(name)
         self.RA = float(ra)
         self.DEC = float(dec)
 
 def HMS2deg(ra='', dec=''):
+
   RA, DEC, rs, ds = '', '', 1, 1
+
   if dec:
     D, M, S = [float(i) for i in dec.split()]
     if str(D)[0] == '-':
@@ -36,48 +40,56 @@ ned.readline()
 
 range = 1
 
-
-
 NEDList = []
+
 FermiList = []
 
 for line in ned:
+
     line=re.sub(' +',' ',line)
+
     RA = re.sub('h|m|s',' ',line.split()[3])
     DEC = re.sub('d|m|s',' ',line.split()[4])
     NAME = line.split()[2]
+
     NEDList.append(GRB(NAME,HMS2deg(RA,DEC)[0],HMS2deg(RA,DEC)[1]))
 
-
-
 for line in fermi:
+
     line = re.sub(' +',' ',line)
     line = re.sub(',',' ',line)
     line = re.sub('\)',' ',line)
     line = re.sub('}',' ',line)
+
     RA=line.split()[0]
     DEC = line.split()[1]
     NAME = line.split()[2]
+
     FermiList.append(GRB(NAME,RA,DEC))
 
 results=0
 
 print('NED OBJECT -> FERMI OBJECT')
 
-
 list3 = []
 for NED in NEDList:
+
     list = []
     list2=[]
+
     for Fermi in FermiList:
         list.append(((NED.RA-Fermi.RA)**2+(NED.DEC-Fermi.DEC)**2)**.5)
 
     print(NED.NAME,'-> ',end='')
+
     b = False
+
     for l in list:
+
         if l <= (2*range)**.5 and b:
             print(' AND ',FermiList[list.index(l)].NAME,end='')
             list2.append(FermiList[list.index(l)].NAME)
+
         if l <= (2*(range))**.5:
             print(FermiList[list.index(l)].NAME,end='')
             results+=1
@@ -95,12 +107,17 @@ for NED in NEDList:
             s+=q+', '
         s=s[:-2]
         list3.append(s)
+
 print('---------------------------------------------------------------------------')
+
 print(results,'HAVE RESULTS WITHIN RANGE OF',range,'DEGREE',end='')
+
 if range>1:
     print('s')
 else:
     print('')
+
 print('---------------------------------------------------------------------------')
+
 for r in list3:
     print(r)
